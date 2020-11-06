7c519bb68b19eeb95aef4b0ea72e7020e8b9ab78
Sergei Stepanov <serge-st@yandex.ru>
2020-11-06 23:51:13 +0300

    finished the fourth section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
     cross = document.querySelector('.menu__cross'),
-    menu = document.querySelector('.menu');
+    menu = document.querySelector('.menu'),
+    percent = document.querySelectorAll('.skills__scale-percent'),
+    lines = document.querySelectorAll('.skills__scale-line span');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,4 +10,8 @@ hamburger.addEventListener('click', () => {
 
 cross.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+percent.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
